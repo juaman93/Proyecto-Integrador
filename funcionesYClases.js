fdf98bb97df6276ad22efadd7646691939099c67
juaman93 <75991554+juaman93@users.noreply.github.com>
2022-06-24 11:50:39 -0500

    Update funcionesYClases.js

--- funcionesYClases.js
+++ funcionesYClases.js
@@ -1,3 +1,10 @@
+/**
+ * Clase para crear objetos de categorías
+ * @param  {Int}    idCategoria Número identificador
+ * @param  {String} nombreCategoria Nombre descriptivo
+ * @param  {Int}    montoCategoria Monto del gasto acumulado realizado en esta categoría
+ *
+ */
 class Categoria {
   constructor(idCategoria, nombreCategoria, montoCategoria) {
     this.idCategoria = idCategoria;
@@ -6,10 +13,17 @@ class Categoria {
   }
 }
 
+/**
+ * Clase para crear gastos individuales
+ * @param  {Int}    idGasto Número identificador
+ * @param  {String} nombreCategoria Nombre de la categoría en el que se hizo el gasto
+ * @param  {Int}    montoGasto Monto del gasto realizado
+ *
+ */
 class Gasto {
   constructor(idGasto, nombreCategoria, montoGasto) {
     this.idGasto = idGasto;
-    this.nombreCategoria = nombreCategoria;
+    this.nombreGasto = nombreCategoria;
     this.montoGasto = montoGasto;
   }
 }
@@ -21,7 +35,9 @@ let formatter = new Intl.NumberFormat("en-US", {
   minimumFractionDigits: 0,
 });
 
-/* Push de categorías iniciales al arreglo de categorías */
+/**
+ * Función para cargar las categorías iniciales predeterminadas
+ */
 function cargarCategoriasPredeterminadas() {
   listaCategorias.push(new Categoria(1, "Ocio", 0));
   listaCategorias.push(new Categoria(2, "Mercado", 0));
@@ -30,6 +46,10 @@ function cargarCategoriasPredeterminadas() {
   listaCategorias.push(new Categoria(5, "Educación", 0));
 }
 
+/**
+ * Función para devolver el nombre de una categoría de acuerdo al número identificador de categoría proporcionado
+ * @param  {Int} idCategoria Número identificador
+ */
 function traerNombreCategoria(idCategoria) {
   return listaCategorias[idCategoria - 1].nombreCategoria;
 }
